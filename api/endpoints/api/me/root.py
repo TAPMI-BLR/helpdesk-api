ebfd5a0a1a0013003df41279bea0ccfb79cacab5
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-10-25 14:35:19 +0530

    User Info API Endpoint

--- api/endpoints/api/me/root.py
+++ api/endpoints/api/me/root.py
@@ -1,7 +1,9 @@
+from mayim import Mayim
 from sanic import Request, json
 from sanic.views import HTTPMethodView
 from api.decorators.require_role import require_role
 from api.decorators.require_login import require_login
+from api.mayim.user_executor import UserExecutor
 from api.models.internal.jwt_data import JWT_Data
 
 
@@ -10,5 +12,11 @@ class MeRoot(HTTPMethodView):
     @require_login()
     @require_role(required_role="user", allow_higher=True)
     async def get(self, request: Request, jwt_data: JWT_Data):
-        # return json(jwt_data.to_dict())
-        return json({"error": "Not implemented"}, status=501)
+        # Get User Executor
+        executor = Mayim.get(UserExecutor)
+
+        # Get the User
+        user = await executor.get_user_by_id(jwt_data.uuid)
+
+        # Return the User
+        return json({"status": "success", "user": user.to_dict()})
