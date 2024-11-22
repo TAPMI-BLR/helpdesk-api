2d3c8c8c78b8f496f3922a184d4ab40f04c9f5ee
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-11-22 22:02:59 +0530

    Modify get_messages query to include author details and specific fields to match new model

--- api/mayim/queries/messages/get_messages.sql
+++ api/mayim/queries/messages/get_messages.sql
@@ -1,5 +1,18 @@
-SELECT *
-FROM messages
-WHERE ticket_id = $ticket_id
-ORDER BY created_at DESC
-LIMIT $limit OFFSET $offset;
+SELECT
+    m.id,
+    m.created_at,
+    m.type,
+    m.ticket_id,
+    row_to_json((SELECT u FROM (SELECT u.id, u.name) u)) AS author,
+    m.content,
+    m.file_id
+FROM
+    messages m
+JOIN
+    users u ON u.id = m.author_id
+WHERE
+    m.ticket_id = $ticket_id
+ORDER BY
+    m.created_at DESC
+LIMIT
+    $limit OFFSET $offset;
