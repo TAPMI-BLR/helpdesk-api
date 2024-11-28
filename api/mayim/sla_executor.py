1f958eb0f7cf1ec85aaf444695dece6633bf472d
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-11-28 22:50:18 +0530

    Plan queries for Team and SLA executors

--- api/mayim/sla_executor.py
+++ api/mayim/sla_executor.py
@@ -1,6 +1,18 @@
+from typing import List
 from mayim import PostgresExecutor
+
+from api.models.db.sla import SLA
 
 
 class SLAExecutor(PostgresExecutor):
     generic_prefix = ""
     path = "./queries/SLAs/"
+
+    async def get_all_slas(self, limit: int = 10, offset: int = 0) -> List[SLA]:
+        """Get all SLAs"""
+
+    async def get_sla_by_id(self, sla_id: int) -> SLA:
+        """Get an SLA by its ID"""
+
+    async def create_sla(self, name: str, note: str):
+        """Create an SLA"""
--- api/mayim/team_executor.py
+++ api/mayim/team_executor.py
@@ -1,6 +1,16 @@
+from uuid import UUID
 from mayim import PostgresExecutor
 
 
 class TeamExecutor(PostgresExecutor):
     generic_prefix = ""
     path = "./queries/teams/"
+
+    async def get_members_by_category_id(self, category_id: UUID):
+        """Get all members by category"""
+
+    async def add_member_to_category_team(self, category_id: UUID, user_id: UUID):
+        """Add a member to a team"""
+
+    async def remove_member_from_category_team(self, category_id: UUID, user_id: UUID):
+        """Remove a member from a team"""
