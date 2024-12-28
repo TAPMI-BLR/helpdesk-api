fee3bceb6d6aa403cf7a9d0f566caa93be3b5c3c
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-12-28 19:22:43 +0530

    Implement severity levels retrieval

--- api/endpoints/api/options/severity.py
+++ api/endpoints/api/options/severity.py
@@ -1,8 +1,17 @@
-from sanic import json
+from mayim import Mayim
+from sanic import Request, json
 from sanic.views import HTTPMethodView
+
+from api.decorators.require_login import require_login
+from api.decorators.require_role import require_role
+from api.mayim.severity_executor import SeverityExecutor
 
 
 class SeverityOptions(HTTPMethodView):
-    # TODO
-    async def get(self, request):
-        return json({"error": "Not implemented"}, status=501)
+    @require_login()
+    @require_role(required_role="team", allow_higher=True)
+    async def get(self, request: Request):
+        severity_executor = Mayim.get(SeverityExecutor)
+        severities = await severity_executor.get_all_severity_levels()
+        severities = [severity.to_dict() for severity in severities]
+        return json({"levels": severities})
