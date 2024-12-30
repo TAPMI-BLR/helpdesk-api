7d705565613809c0a2a4c93d05ab57165dd26af1
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-12-30 23:52:14 +0530

    Implement SLA options retrieval with login and role requirements

--- api/endpoints/api/options/sla.py
+++ api/endpoints/api/options/sla.py
@@ -1,8 +1,17 @@
-from sanic import json
+from mayim import Mayim
+from sanic import Request, json
 from sanic.views import HTTPMethodView
+
+from api.decorators.require_login import require_login
+from api.decorators.require_role import require_role
+from api.mayim.sla_executor import SLAExecutor
 
 
 class SLAOptions(HTTPMethodView):
-    # TODO
-    async def get(self, request):
-        return json({"error": "Not implemented"}, status=501)
+    @require_login()
+    @require_role(required_role="team", allow_higher=True)
+    async def get(self, request: Request):
+        sla_executor = Mayim.get(SLAExecutor)
+        sla_options = await sla_executor.get_all_slas()
+        sla_options = [sla.to_dict() for sla in sla_options]
+        return json({"slas": sla_options})
