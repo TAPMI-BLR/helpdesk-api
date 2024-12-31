6fd7d5d416bbf0722d9aeeceb3c4c5ec5da9ffb8
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-12-31 13:37:02 +0530

    Implement Category options retrieval with login and role requirements

--- api/endpoints/api/options/category.py
+++ api/endpoints/api/options/category.py
@@ -1,8 +1,25 @@
-from sanic import json
+from mayim import Mayim
+from sanic import Request, json
 from sanic.views import HTTPMethodView
+from sanic_ext import validate
+
+from api.decorators.require_login import require_login
+from api.mayim.category_executor import CategoryExecutor
+from api.models.requests.category_options_query import CategoryOptionsQuery
 
 
 class CategoryOptions(HTTPMethodView):
-    # TODO
-    async def get(self, request):
-        return json({"error": "Not implemented"}, status=501)
+    @validate(query=CategoryOptionsQuery)
+    @require_login()
+    async def get(self, request: Request, query: CategoryOptionsQuery):
+        category_executor = Mayim.get(CategoryExecutor)
+        options = []
+        if query.category_id:
+            options = await category_executor.get_subcategories(
+                parent_id=query.category_id
+            )
+        else:
+            options = await category_executor.get_categories()
+
+        options = [option.to_dict() for option in options]
+        return json({"options": options})
---
+++ api/models/requests/category_options_query.py
@@ -0,0 +1,8 @@
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel
+
+
+class CategoryOptionsQuery(BaseModel):
+    category_id: Optional[UUID] = None
