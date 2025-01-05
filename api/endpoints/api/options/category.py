b0313db98411d43e473d93148bd5af93e736df92
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2025-01-05 11:23:16 +0530

    Add missing JWT_Data Parameter which is auto injected via decorators
    Add required min role for category options

--- api/endpoints/api/options/category.py
+++ api/endpoints/api/options/category.py
@@ -4,14 +4,19 @@ from sanic.views import HTTPMethodView
 from sanic_ext import validate
 
 from api.decorators.require_login import require_login
+from api.decorators.require_role import require_role
 from api.mayim.category_executor import CategoryExecutor
+from api.models.internal.jwt_data import JWT_Data
 from api.models.requests.category_options_query import CategoryOptionsQuery
 
 
 class CategoryOptions(HTTPMethodView):
     @validate(query=CategoryOptionsQuery)
     @require_login()
-    async def get(self, request: Request, query: CategoryOptionsQuery):
+    @require_role(required_role="user", allow_higher=True)
+    async def get(
+        self, request: Request, jwt_data: JWT_Data, query: CategoryOptionsQuery
+    ):
         category_executor = Mayim.get(CategoryExecutor)
         options = []
         if query.category_id:
--- api/endpoints/api/options/sla.py
+++ api/endpoints/api/options/sla.py
@@ -5,12 +5,13 @@ from sanic.views import HTTPMethodView
 from api.decorators.require_login import require_login
 from api.decorators.require_role import require_role
 from api.mayim.sla_executor import SLAExecutor
+from api.models.internal.jwt_data import JWT_Data
 
 
 class SLAOptions(HTTPMethodView):
     @require_login()
     @require_role(required_role="team", allow_higher=True)
-    async def get(self, request: Request):
+    async def get(self, request: Request, jwt_data: JWT_Data):
         sla_executor = Mayim.get(SLAExecutor)
         sla_options = await sla_executor.get_all_slas()
         sla_options = [sla.to_dict() for sla in sla_options]
